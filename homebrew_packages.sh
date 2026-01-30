6e835db6e4a01b077a1acf484f3ff06fffebc135
dtamien <76497025+dtamien@users.noreply.github.com>
2026-01-30 18:44:44 +0100

    Update homebrew_packages.sh with new packages
    Added additional Homebrew packages to the installation script.

--- homebrew_packages.sh
+++ homebrew_packages.sh
@@ -1,10 +1,48 @@
 # Installs a list of Homebrew packages
 
 packages=(
+    azure-cli
+    baobab
+    coreutils
+    ffmpeg
+    fzf
+    git
+    git-lfs
+    go-task/tap/go-task
+    hashicorp/tap/terraform
+    htop
+    hunspell
+    imagemagick
+    jq
     lazygit
+    libpq
+    livekit-cli
+    md2pdf
+    mkcert
+    neovim
+    nmap
+    node
+    nss
+    ollama
+    pandoc
+    pdfly
     pipx
+    pkgconf
+    postgresql@14
     pre-commit
+    python@3.10
+    python@3.11
+    python@3.12
+    python@3.9
+    ranger
+    s3cmd
+    samba
+    tflint
+    tree
+    usbutils
     uv
+    wget
+    yq
 )
 
 for pkg in "${packages[@]}"; do
